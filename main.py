e116bc2ec8bddfc2ff522d345dfc9a0201527046
Sebastian <haggblca@arcada.fi>
2023-09-19 15:00:57 +0300

    added final countdown

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # This is a sample Python script.
+import math
+from random import random
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-
+x = 10
 print("Hello World")
 
+print(random())
+
+for x in range(10,0,-1):
+    print(x)
